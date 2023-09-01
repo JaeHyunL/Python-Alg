92815258d7095fbee0f958da3f92b3d1cd511f72
JaeHyunL <swaa23@gmail.com>
2023-09-01 10:23:13 +0900

    뇌풀기..

---
+++ re_try_study;;/level1약수의합.py
@@ -0,0 +1,27 @@
+"""
+정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+
+제한 사항
+n은 0 이상 3000이하인 정수입니다.
+입출력 예
+n	return
+12	28
+5	6
+입출력 예 설명
+입출력 예 #1
+12의 약수는 1, 2, 3, 4, 6, 12입니다. 이를 모두 더하면 28입니다.
+
+입출력 예 #2
+5의 약수는 1, 5입니다. 이를 모두 더하면 6입니다.
+"""
+
+def solution(TARGET_NUMBER):
+
+    SUM = TARGET_NUMBER
+    for i in range(1, (TARGET_NUMBER // 2) + 1):
+        if TARGET_NUMBER % i == 0:
+            SUM += i
+
+    return SUM
+
+print(solution(5))
